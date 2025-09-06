446278f26fe3802bcce3f1e72490abfef380f400
amandeep25 <st.amandeep25@gmail.com>
2025-09-07 00:43:50 +0530

    Some modification in navbar

--- Frontend/src/components/LatestCollection.jsx
+++ Frontend/src/components/LatestCollection.jsx
@@ -13,7 +13,7 @@ useEffect(()=>{
 
 
   return (
-    <div className='my-10'>
+    <div className='my-10 mx-6'>
       <div className="text-center py-8 text-3xl">
         <Title text1= {'LATEST'} text2={'COLLECTIONS'}/>
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
