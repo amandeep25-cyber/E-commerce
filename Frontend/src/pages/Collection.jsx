00593322e4e0d59d8167c3892680da02e938ce8b
amandeep25 <st.amandeep25@gmail.com>
2025-09-10 12:36:11 +0530

    Added all fuctionalities in the collection page

--- Frontend/src/pages/Collection.jsx
+++ Frontend/src/pages/Collection.jsx
@@ -11,6 +11,7 @@ const Collection = () => {
   const [filterProducts ,setFilterProducts] = useState([])
   const [category, setCategory] = useState([]);
   const [subcategory, setSubCategory] = useState([])
+  const [sortType,setSortType] = useState('relavent')
 
   const toggleCategory = (e)=>{
     
@@ -21,9 +22,47 @@ const Collection = () => {
     }
   }
 
+  const toggleSubCategory = (e) =>{
+    if(subcategory.includes(e.target.value)){
+      setSubCategory(prev => prev.filter(item=> item!==e.target.value))
+    }else{
+      setSubCategory(prev => [...prev,e.target.value])
+    }
+  }
+
+  const applyFilter = () =>{
+    let productsCopy = products.slice();
+    if(category.length >0){
+      productsCopy = productsCopy.filter(item=> category.includes(item.category));
+    }
+    if(subcategory.length>0){
+      productsCopy = productsCopy.filter(item=> subcategory.includes(item.subCategory))
+    }
+    setFilterProducts(productsCopy)
+  }
+
+ const sortProduct = ()=>{
+  let fpCopy = filterProducts.slice();
+  switch(sortType){
+    case 'low-high':
+      setFilterProducts(fpCopy.sort((a,b)=>(a.price - b.price)))
+      break;
+    case 'high-low':
+      setFilterProducts(fpCopy.sort((a,b)=>(b.price - a.price)))
+      break;
+    default:
+      applyFilter();
+      break;
+  }
+ }
+
   useEffect(()=>{
-    setFilterProducts(products);
-  },[])
+    applyFilter();
+  },[category,subcategory])
+
+  useEffect(()=>{
+    sortProduct();
+  },[sortType])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t mx-6'>
@@ -37,13 +76,13 @@ const Collection = () => {
           <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className='flex gap-2'>
-              <input type="checkbox" value={'Men'} className='w-3' /> Men
+              <input type="checkbox" value={'Men'} className='w-3' onChange={toggleCategory} /> Men
             </p>
             <p className='flex gap-2'>
-              <input type="checkbox" value={'Women'} className='w-3' /> Women
+              <input type="checkbox" value={'Women'} className='w-3' onChange={toggleCategory} /> Women
             </p>
             <p className='flex gap-2'>
-              <input type="checkbox" value={'Kids'} className='w-3' /> Kids
+              <input type="checkbox" value={'Kids'} className='w-3' onChange={toggleCategory} /> Kids
             </p>
           </div>
 
@@ -53,13 +92,13 @@ const Collection = () => {
           <p className='mb-3 text-sm font-medium'>Type</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className='flex gap-2'>
-              <input type="checkbox" value={'Topwear'} className='w-3' /> Topwear
+              <input type="checkbox" value={'Topwear'} onChange={toggleSubCategory} className='w-3' /> Topwear
             </p>
             <p className='flex gap-2'>
-              <input type="checkbox" value={'Bottomwear'} className='w-3' /> Bottomwear
+              <input type="checkbox" value={'Bottomwear'} onChange={toggleSubCategory} className='w-3' /> Bottomwear
             </p>
             <p className='flex gap-2'>
-              <input type="checkbox" value={'Winterwear'} className='w-3' /> Winterwear
+              <input type="checkbox" value={'Winterwear'} onChange={toggleSubCategory} className='w-3' /> Winterwear
             </p>
           </div>
 
@@ -70,7 +109,7 @@ const Collection = () => {
         <div className="flex justify-between text-base sm:text-2xl mb-4">
             <Title text1={'ALL'} text2={'COLLECTIONS'}/>
             {/* Product sort */}
-            <select className='border-2 border-gray-300 text-sm px-2'>
+            <select onChange={(e)=>setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
               <option value="relavent">Sort by: Relavent</option>
               <option value="low-high">Sort by: Low to High</option>
               <option value="high-low">Sort by: High to Low</option>
