d020979a44ee4296a1fd2b8fc69be304eeda6227
amandeep25 <st.amandeep25@gmail.com>
2025-10-13 11:35:11 +0530

    Cart controllers updated

--- backend/controllers/cart.controllers.js
+++ backend/controllers/cart.controllers.js
@@ -1,18 +1,69 @@
-
+import userModel from '../models/user.model.js';
 
 const addToCart = async(req, res) => {  
-    // Logic to add item to cart
-    res.status(200).send('Item added to cart');
+    try {
+        const { userId, itemId, size} = req.body;
+
+        const userData = await userModel.findById(userId);
+        let cartData = await userData.cartData;
+
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size] +=1;
+            }else{
+                cartData[itemId][size]=1;
+            }
+        }else{
+            cartData[itemId] = {}
+            cartData[itemId][size]=1;
+        }
+
+        await userModel.findByIdAndUpdate(userId,{cartData})
+        res.json({success:true,message:'Added to cart'})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
 }   
 
 const updateCartItem = async(req, res) => {  
-    // Logic to update item in cart
-    res.status(200).send('Cart item updated');
+    
+    try {
+        
+        const { userId,itemId,size,quantity} = req.body;
+
+        const userData = await userModel.findById(userId)
+
+        let cartData = await userData.cartData
+
+        cartData[itemId][size]= quantity
+
+        await userModel.findByIdAndUpdate(userId,{cartData})
+
+        res.json({success:true, message:"Cart updated"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
 }
 
 const getCart = async(req, res) => {  
-    // Logic to get cart items
-    res.status(200).send('Cart items retrieved');
+    try {
+        
+        const { userId } = req.body
+        
+        const userData = await userModel.findById(userId)
+
+        let cartData = await userData.cartData
+
+        res.json({success:true,cartData})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
 }
 
 export { addToCart, updateCartItem, getCart };
